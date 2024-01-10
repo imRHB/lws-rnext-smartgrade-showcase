32e796e5946f9669dbe6f781b6a10d41265403d5
rhbabu3@gmail.com <rhbabu3@gmail.com>
2024-01-10 12:42:28 +0600

    html converted to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 // import "./App.css";
 
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Students from "./components/Students";
+
 function App() {
     return (
         <div>
-            <h1 className="text-3xl font-bold">
+            <Navbar />
+            <Hero />
+            <Students />
+            <Footer />
+            {/* <h1 className="text-3xl font-bold">
                 SmartGrade Showcase | Learn with Sumit
-            </h1>
+            </h1> */}
         </div>
     );
 }
---
+++ src/components/Students.jsx
@@ -0,0 +1,21 @@
+import Search from "./Search";
+import StudentList from "./StudentList";
+
+export default function Students() {
+    return (
+        <section className="py-24 lg:pt-[120px] lg:pb-28">
+            <div className="container">
+                <div className="mb-16 flex flex-col items-center">
+                    <h2 className="text-3xl lg:text-[40px] mb-9 font-bold">
+                        <span className="text-[#00CC8C]">Students</span> of the
+                        Year
+                    </h2>
+
+                    <Search />
+                </div>
+
+                <StudentList />
+            </div>
+        </section>
+    );
+}
