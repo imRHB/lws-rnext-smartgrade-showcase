3bc082e99e0827ff97f12a8e23b36faa6030b955
rhbabu3@gmail.com <rhbabu3@gmail.com>
2024-01-10 12:20:13 +0600

    smart grade showcase project initialized

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import "./App.css";
+
+function App() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold">
+                SmartGrade Showcase | Learn with Sumit
+            </h1>
+        </div>
+    );
+}
+
+export default App;
